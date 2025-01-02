6dd67dac9c865a812fbeac1071a152ca6959a6c8
Magnus <nharnharyhaw984@gmail.com>
2025-01-02 19:49:10 +0000

    change server port from 5000 to 3000

--- app.py
+++ app.py
@@ -224,4 +224,4 @@ if __name__ == "__main__":
     """ Main Function """
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
